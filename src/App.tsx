1c6bdf612becd86f19fa55f90ddb24a5c9b61449
quranicphrases <quranicphrases@users.noreply.github.com>
2025-11-24 10:17:50 +0530

    version 0.96
    added hash router and fixed resource url in fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Navigate,
@@ -23,7 +23,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename={import.meta.env.DEV ? '/' : '/quranic-phrases/'}>
+      <Router>
         <Box
           sx={{
             minHeight: '100vh',
